ce88f002e8f8fd149e20ad67bdaa635db5307c15
Blendi Rrustemi <blendirrustemi9@gmail.com>
2022-04-07 14:54:19 +0200

    Sign Up Page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 # Create your views here.
+def sign_up(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog-index')
+
+    else:
+        form = SignUpForm()
+        
+    form = SignUpForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'users/signup.html', context)
