53a9ed8638ee010e6b5c83aba03001065c38a998
Blendi Rrustemi <blendirrustemi9@gmail.com>
2022-04-10 20:03:29 +0200

    Removed Signup added About Me

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from .import views
 from django.contrib.auth import views as auth_view
 
 urlpatterns = [
-    path('sign_up/', views.sign_up, name='users-sign-up'),
+    path('about_me/', views.about_me, name='users-about_me'),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'),
          name='users-login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'),
--- users/views.py
+++ users/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
 
 # Create your views here.
-def sign_up(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('blog-index')
-
-    else:
-        form = SignUpForm()
-        
-    form = SignUpForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'users/signup.html', context)
+def about_me(request):
+    return render(request, 'users/about_me.html')
